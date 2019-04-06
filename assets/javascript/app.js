595a7b58614101d5dcec67a8b12f86d418bd25e9
Gabriel Tunggala <gtunggala@gmail.com>
2019-04-06 12:53:15 -0400

    js and html skeleton, gifs working

---
+++ assets/javascript/app.js
@@ -0,0 +1,112 @@
+
+
+// HOST
+// api.giphy.com
+
+// PATH
+// GET /v1/gifs/search
+
+// key: tLhhmbIr7oigYS7Q6RvF9zhGa48cHBic
+
+// search parameters:
+// q= rainbow
+
+
+
+
+var api= "https://api.giphy.com/v1/gifs/search?";
+var apiKey= "&api_key=tLhhmbIr7oigYS7Q6RvF9zhGa48cHBic";
+var query= "&q=rainbow";
+
+var availableAtl = [
+    'Atlanta',
+    'Hip-hop',
+    'Peach',
+    'Real Housewives of Atlanta',
+    'Atlanta UNITED',
+    'Atlanta Falcons',
+    'Atlanta Hawks',
+    'Bird scooter',
+    'Outkast',
+    'Pink Trap house',
+    'Murder Kroger',
+    'Tiny Doors ATL',
+    'Donald Glover',
+    'Chicken wings',
+    'Traffic',
+    'Atlanta FX',
+    'Teddy Perkins',
+    'Buford Highway',
+    'Snowpocalypse',
+    'Georgia',
+    'In the Perimeter',
+    'Buckhead',
+    'Dirty south',
+    'Shane Company'
+]
+
+function renderAtlButton() {
+    $('#buttons-view').empty();
+
+    for (var i= 0; i < availableAtl.length; i++) {
+        console.log(availableAtl[i]);
+
+        const button= $('<button>'); //dynamically creating buttons
+        button.addClass('atlBtn');
+        button.attr("dataname", availableAtl[i]); 
+        button.text(availableAtl[i]); //text for buttons
+        $('#buttons-view').append(button);
+    }
+
+}
+
+function setup() {
+
+    const gif= $(this).attr('dataname');
+    const queryURL= 'https://api.giphy.com/v1/gifs/search?q=' + gif + '&api_key=tLhhmbIr7oigYS7Q6RvF9zhGa48cHBic' + '&limit=10';
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response) {
+        console.log(response);
+
+        $('.atlanta').empty(); //clears the .atlanta div
+        var results= response.data;
+        console.log (results);
+        const gifDiv= $('<div>');
+
+        for (var i= 0; i < results.length; i++) {
+            
+            const atlRating= results[i].rating;
+            console.log(atlRating);
+
+            const displayatlRating= $('<p>').text('Rated" ' + atlRating);
+            const atlImgURL= results[i].images.downsized_large.url;
+            console.log(atlImgURL);
+
+            const atlGifImg= $('<img>').attr('src', atlImgURL);
+
+            gifDiv.prepend(displayatlRating);
+            gifDiv.prepend(atlGifImg);
+        }
+
+        $('#atlGIFview').html(gifDiv);
+    })
+};
+$('#find-atlanta').on('click', function() {
+    event.preventDefault();
+    var newButton= $('#atlanta-input').val().trim();
+    availableAtl.push(newButton);
+    renderAtlButton();
+    console.log(availableAtl);
+    console.log('mohsi moshi')
+});
+
+//$('.atlBtn').on('click', setup());
+
+
+
+$(document).on('click', '.atlBtn', setup);
+
+renderAtlButton();
